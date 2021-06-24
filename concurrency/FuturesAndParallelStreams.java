a6a621c9d956125aa11bc3b07e0d73f91061f4d5
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2021-06-24 19:58:51 +0530

    Included Futures,  Streams,ThreadLocalRandom

--- concurrency/FuturesAndParallelStreams.java
+++ concurrency/FuturesAndParallelStreams.java
@@ -2,6 +2,7 @@ package concurrency;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -30,6 +31,15 @@ Future:
 
 5. Future.cancel(true): to tell the executor to stop the operation and interrupt its underlying thread.  
 
+CompletableFuture
+-----------------
+1. CompletableFuture class introduced in Java 8. It supports various Functional interfaces such as
+    Runnable, Consumer, Supplier etc. Further Java 9 added additional functionality such as
+    new Factory methods, support for delays and timeouts, improved support for subclassing. 
+
+2. Biggest advantage of CompletableFuture is that we can combine CompletableFuture instances in a chain 
+    of computation steps.
+
 Parallel Streams:
 -----------------
 1. Java 8 introduced concept of Streams as an efficient way of carrying out bulk operations on data. 
@@ -42,8 +52,9 @@ public class FuturesAndParallelStreams {
     
     
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        simulateFutures();
-        simulateParallelStreams();
+        //simulateFutures();
+        //simulateParallelStreams();
+        simulateCompletableFuture();
     }
 
     /**
@@ -57,6 +68,70 @@ public class FuturesAndParallelStreams {
         }
         Integer result = future.get();
         System.out.println("Result: " + result);
+    }
+
+    /**
+     * Simulates all possible combinations of CompletableFuture class
+     */
+    static void simulateCompletableFuture() throws InterruptedException, ExecutionException {
+        Future<String> result = calculateAsync();
+        System.out.println(result.get());
+
+        // // if we already know the result of computation
+        // Future<String> completableFuture = CompletableFuture.completedFuture("Hello");
+        // System.out.println(completableFuture.get());
+
+        // // Uses Supplier Interface get() method which returns the value
+        // CompletableFuture<String> anotherCompletableFuture = CompletableFuture.supplyAsync( () -> "Hello" );
+        // CompletableFuture<String> anotherFuture = anotherCompletableFuture.thenApply(value -> value + " World!");
+        // System.out.println(anotherFuture.get());
+
+        // CompletableFuture<Void> consumerFuture = anotherCompletableFuture.thenAccept(
+        //                                             value -> System.out.println("Computation Returned: " + value));
+        // System.out.println(consumerFuture.get());
+
+        // CompletableFuture<Void> runnableFuture = anotherCompletableFuture.thenRun(
+        //                                             () -> System.out.println("Computation Finished"));
+        // System.out.println(runnableFuture.get());
+
+        // CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello")
+        //                                                             .thenCompose(value -> CompletableFuture.supplyAsync(() -> value + " World"));
+
+        // CompletableFuture<String> completableFuture   = CompletableFuture.supplyAsync(() -> "Hello")
+        //                                                                 .thenCombine(
+        //                                                                     CompletableFuture.supplyAsync(
+        //                                                                                 () -> " World"), (s1, s2) -> s1 + s2);
+
+        // CompletableFuture completableFuture = CompletableFuture.supplyAsync(() -> "Hello")
+        //                                             .thenAcceptBoth(CompletableFuture.supplyAsync(() -> " World"),
+        //                                                                                   (s1, s2) -> System.out.println(s1 + s2));                                                                                        
+
+        // System.out.println(completableFuture.get());    
+
+        // Running multiple Futures in parallel
+        // CompletableFuture<String> future1  = CompletableFuture.supplyAsync(() -> "Hello");
+        // CompletableFuture<String> future2  = CompletableFuture.supplyAsync(() -> "Beautiful");
+        // CompletableFuture<String> future3  = CompletableFuture.supplyAsync(() -> "World");
+        // CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(future1, future2, future3);
+        // System.out.println(future1.isDone());
+        // System.out.println(future2.isDone());
+        // System.out.println(future3.isDone());        
+        // System.out.println(combinedFuture.get());
+
+        //exception handling
+        // String name = null;
+        // CompletableFuture<String> completableFuture  =  CompletableFuture.supplyAsync(() -> {
+        //                                                         if (name == null) {
+        //                                                             throw new RuntimeException("Computation error!");
+        //                                                         }
+        //                                                         return "Hello, " + name;
+        //                                                 }).handle((s, t) -> s != null ? s : "Hello, Stranger!");
+        // System.out.println(completableFuture.get());
+
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        completableFuture.completeExceptionally(new RuntimeException("Calculation failed!"));
+        completableFuture.get(); // ExecutionException
     }
 
     /**
@@ -76,6 +151,16 @@ public class FuturesAndParallelStreams {
         finally {
             pool.shutdown();
         }
+    }
+
+    static Future<String> calculateAsync() {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        Executors.newCachedThreadPool().submit(() -> { 
+            TimeUnit.MILLISECONDS.sleep(300);
+            future.complete("Hello");
+            return null;
+        });
+        return future;
     }
 
     static Future<Integer> calculate(Integer value) {
---
+++ concurrency/ThreadLocalRandomzier.java
@@ -0,0 +1,31 @@
+package concurrency;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+1. ThreadLocalRandom introduced in Java 7 and it is for generating random numbers in multi-threaded
+    environment.
+
+2. Random class is not thread-safe meaning doesn't perform well in multi-threaded environment. It is
+    due to contention meaning given that multiple threads share same Random instance. Because Random
+    provides numbers globally. Random allows setting seed explicitly.
+
+3. ThreadLocalRandom is a combination of ThreadLocal and Random classes and isolated to the current
+    thread. Thus it achieves better performance in a multi-threaded environment by simply avoiding
+    any concurrent access to instances of Random. ThreadLocalRandom does not allow setting seed 
+    explicitly. ThreadLocalRandom is more efficient in a highly concurrent environment.
+*/
+public class ThreadLocalRandomzier {
+    public static void main(String[] args) {
+        Integer unboundedRandomValue = ThreadLocalRandom.current().nextInt();
+        // 0 is inclusive lower limit and 100 is exclusive upper limit
+        Integer boundedRandomValue = ThreadLocalRandom.current().nextInt(0, 100);
+        System.out.println("Intger: unbounded: " + unboundedRandomValue + ", bounded: " + boundedRandomValue);
+        // Java 8 introduced nextGaussian()
+        // nextGaussian(): generates next normally distributed value with 0.0 (mean) and 1.0 (standard deviation)
+        Double distributedValue = ThreadLocalRandom.current().nextGaussian();
+        System.out.println("Distributed value: " + distributedValue);
+    }
+    
+    
+}
